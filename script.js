809d2ddc329afde6794149964b0b3545cd94a1c4
Hannah Wandiri <wandirihannah@gmail.com>
2026-07-28 12:04:26 +0300

    Add input validation

--- script.js
+++ script.js
@@ -47,3 +47,28 @@ form.addEventListener('submit', function (event) {
         const gender 
         = document.querySelector('input[name="gender"]:checked');
     });
+
+    if (day=="" || month=="" || year=="" ) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    if (day < 1 || day > 31) {
+        alert("Please enter a valid day (1-31).");
+        return;
+    }
+
+    if (month < 1 || month > 12) {
+        alert("Please enter a valid month (1-12).");
+        return;
+    }
+
+    if (year < 1000 || year > 9999) {
+        alert("Please enter a valid year (4 digits).");
+        return;
+    }
+
+    if (!gender) {
+        alert("Please select a gender.");
+        return;
+    }
