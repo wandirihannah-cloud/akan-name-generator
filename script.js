f930d626d445a21aa8f41c4ca6d7ed7cc1c5040b
Hannah Wandiri <wandirihannah@gmail.com>
2026-07-28 11:39:04 +0300

    Added Akan names

---
+++ script.js
@@ -0,0 +1,19 @@
+const maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+];
+
+const femaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
